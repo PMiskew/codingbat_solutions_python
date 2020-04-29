9738dd064afa4e04f2e6bb5a49cf9a57d775b09c
PMiskew <paul.miskew@gmail.com>
2020-04-28 20:30:28 -0400

    update

---
+++ List1/make_pi.py
@@ -0,0 +1,24 @@
+'''
+
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+
+make_pi() → [3, 1, 4]
+
+
+'''
+
+def make_pi():
+  
+  #dynamic data strucutre: THe size dones't have to be set when first construted
+  
+  '''
+  list = [3,1,4]
+  return list
+  '''
+  
+  list = []
+  list.append(3)
+  list.append(1)
+  list.append(4)
+  return list
