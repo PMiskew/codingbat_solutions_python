e058c32d9325c24e55e0a0bcf104720fe51e16e3
PMiskew <paul.miskew@gmail.com>
2020-05-12 20:50:17 -0400

    update

--- List2/sum13.py
+++ List2/sum13.py
@@ -1,6 +1,16 @@
 def sum13(nums):
-  
-  
+'''
+
+Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+
+
+sum13([1, 2, 2, 1]) → 6
+sum13([1, 1]) → 2
+sum13([1, 2, 2, 1, 13]) → 6
+
+'''
+
+def sum13(nums):
   #
   #         0  1   2  3  4  5
   # nums = [1, 13, 2, 2, 1, 13]  --> 1 + 2 + 1 = 4
@@ -48,7 +58,7 @@ def sum13(nums):
   #
   #
   
-  
+  '''
   sum = 0
   i = 0
   
@@ -62,6 +72,125 @@ def sum13(nums):
     i = i + 1
       
   return sum
+  '''
+
+
+  #This solution was based on a YouTube comment asking why their code
+  #didn't work.  It was using an approximate method to this, but
+  #used a for loop and hand some logic issues.   The big idea here is
+  #is that you CANNOT change a for loop variable once a loop starts. 
+  #The big idea here is that when you remove an element from a list all
+  #elements to the right will shift their index. 
+
+  #
+  #   0  1  2   3  4  5
+  #  [1, 2, 13, 2, 1, 13]
+  #  
+  #  i = 0  --> 0 < 6 TRUE  --> RUN LOOP
+  #    nums[0] == 13 FALSE
+  #      SKIP CONDITIONAL CODE BLOCK   
+  #    sum = sum + nums[0] = 0 + 1
+
+  #  i = 1 --> 1 < 6 TRUE --> RUN LOOP
+  #   nums[0] == 13 FALSE
+  #     SKIP CONDITIONAL CODE BLOCK   
+  #   sum = sum + nums[1] = 1 + 2 = 3
+  
+  
+  #  i = 2 --> 2 < 6 TRUE --> RUN LOOP
+  #   nums[2] == 13 TRUE
+  #     ENTER CONDITIONAL CODE BLOCK
+  #                                        X  X
+  #                               0  1  2  3  4
+  #     nums.remove(nums[2]) --> [1, 2, 2, 1, 13] **INDEX SHIFT, LENGTH CHANGE
+  #   
+  #     2 < 5 TRUE
+  #                                       X  X
+  #                                 0  1  2  3
+  #       nums.remove(nums[2]) --> [1, 2, 1, 13] **INDEX SHIFT, LENGTH CHANGE
+  #
+  #   2 < 4 and nums[i] != 13 TRUE
+  #     sum = sum + nums[i] = 3 + 1 = 4
+  
+  
+  #  i = 3 --> 3 < 4 TRUE --> RUN LOOP
+  #   nums[3] == 13 TRUE
+  #     ENTER CONDITIONAL CODE BLOCK
+  #                       
+  #                               0  1  2 
+  #     nums.remove(nums[3]) --> [1, 2, 1] **LENGTH CHANGE
+  #     
+  #      3 < 3 FALSE
+  #         SKIP CONDITIONAL CODE BLOCK
+  #
+  #  3 < 3 and nums[i] != 13 FALSE
+  #     SKIP CONDITIONAL CODE BLOCK
+  
+  #  i = 4 --> 4 < 3 FALSE --> EXIT LOOP
+  #     
+  # return sum --> 4
+  #
+  
+  if len(nums)== 0:
+    return 0
+    
+  sum=0  
+  i = 0
+  
+  while (i < len(nums)):
+    
+    if nums[i]==13 :
+      
+      nums.remove(nums[i])
+      
+      if i < len(nums): 
+        nums.remove(nums[i])
+  
+    if (i < len(nums) and nums[i] != 13):
+      sum = sum+nums[i]
+    i = i + 1
+  return sum
+
+#THIS IS THE JAVA SOLUTION USED IN MY VIDEO THAT COMPARED PYTHON AND JAVA
+'''
+public int sum13(int[] nums) {
+  
+  if (nums.length == 0) {
+    return 0;
+  }
+  
+  ArrayList<Integer> list = new ArrayList<Integer>();
+  for (int i = 0; i < nums.length; i = i + 1) {
+    list.add(nums[i]);
+  }
+  
+  int sum=0; 
+  int i = 0; //remove if you use for loop
+  
+  //for (int i = 0; i < list.size(); i = i + 1) {
+  while (i < list.size()) {
+    
+    if (list.get(i)==13) {
+      
+      list.remove(i); //remove index i - NOTICE DIFFERENCE FROM PYTHON
+      
+      if (i < list.size()) { 
+        list.remove(i); //remove index i - NOTICE DIFFERENCE FROM PYTHON
+      }
+    }
+    
+    if (i < list.size() && list.get(i) != 13) {
+      sum = sum+ list.get(i);
+    }
+    i = i + 1; //remove if you use for loop
+  }
+  return sum;
+}
+
+
+'''
+
+
   
   
       
---
+++ type_demo.py
@@ -0,0 +1,7 @@
+
+x = "cat" #change this to different type
+
+if type(x) is float:
+	print("IT IS A FLOAT");
+elif type(x) is str:
+	print("IT IS A STRING");
